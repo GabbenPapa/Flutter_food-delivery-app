26f8bd96cc7cd0c75b481b13b21cf8dd91cb997f
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-02-09 17:58:02 +0100

    Add food class and image sources

---
+++ lib/models/food.dart
@@ -0,0 +1,31 @@
+class Food {
+  final String name;
+  final String description;
+  final String imagePath;
+  final double price;
+  final FoodCategory category;
+  List<Addon> availableAddons;
+
+  Food(
+      {required this.name,
+      required this.description,
+      required this.imagePath,
+      required this.price,
+      required this.category,
+      required this.availableAddons});
+}
+
+enum FoodCategory {
+  burgers,
+  salads,
+  sides,
+  desserts,
+  drinks,
+}
+
+class Addon {
+  final String name;
+  final String price;
+
+  const Addon({required this.name, required this.price});
+}
