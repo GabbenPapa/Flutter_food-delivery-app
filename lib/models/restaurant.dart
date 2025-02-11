0a2f324526e36a3235e3f4d8ed7c7288f9d1b22a
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-02-11 09:34:55 +0100

    Add items to restaurants

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -34,13 +34,26 @@ class Restaurant extends ChangeNotifier {
     Food(
       name: "Bacon burger",
       description:
-          "Bacon burger, and lettuce, the meet and cheese is a whole wheat bun.",
+          "Bacon burger and lettuce, the meet and cheese is a whole wheat bun.",
       imagePath: "assets/images/burgers/BaconBurger.jpeg",
       price: 1.99,
       category: FoodCategory.burgers,
       availableAddons: [
         Addon(name: "Extra souse", price: 0.50),
         Addon(name: "Extra cheese", price: 0.50),
+        Addon(name: "Extra lettuce", price: 0.50),
+      ],
+    ),
+    Food(
+      name: "Vega burger",
+      description:
+          "Vega burger with lettuce, toasted buns and our special sauce on a whole wheat bun",
+      imagePath: "assets/images/burgers/VegaBurger.jpeg",
+      price: 3.99,
+      category: FoodCategory.burgers,
+      availableAddons: [
+        Addon(name: "Extra souse", price: 0.50),
+        Addon(name: "Extra vegetables", price: 0.50),
         Addon(name: "Extra lettuce", price: 0.50),
       ],
     ),
@@ -68,6 +81,16 @@ class Restaurant extends ChangeNotifier {
       ],
     ),
     Food(
+      name: "Ice cream bowl",
+      description: "It is a cake selection, with chocolate shake",
+      imagePath: "assets/images/desserts/Desserts.webp",
+      price: 5.50,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra chocolate syrup", price: 1.0),
+      ],
+    ),
+    Food(
       name: "Lager Beer",
       description: "Teasty lager beer, the original recipe from the 1890s",
       imagePath: "assets/images/drinks/Beer.jpeg",
@@ -84,10 +107,68 @@ class Restaurant extends ChangeNotifier {
       availableAddons: [],
     ),
     Food(
+      name: "Cold Drinks",
+      description:
+          "Cold drinks in Fanta, Coke and Sprite, with ice or without ice",
+      imagePath: "assets/images/drinks/ColdDrink.jpeg",
+      price: 2.00,
+      category: FoodCategory.drinks,
+      availableAddons: [],
+    ),
+    Food(
+      name: "Lemonade",
+      description:
+          "Cold lemonade in maracuja, orange, lemon and lime, with ice or without ice",
+      imagePath: "assets/images/drinks/Limonad.jpeg",
+      price: 2.00,
+      category: FoodCategory.drinks,
+      availableAddons: [],
+    ),
+    Food(
+      name: "Eggs Salad",
+      description: "Salad with eggs and lots of vegetables.",
+      imagePath: "assets/images/salads/EggsSalad.jpeg",
+      price: 2.50,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra olives", price: 0.50),
+      ],
+    ),
+    Food(
       name: "Greek Salad",
       description:
-          "It is a giant ice cream bowl with our special toppings, vanilla ice cream, and a whole wheat bun",
+          "Traditional Greek salad with lots of vegetables and olives, feta cheese.",
       imagePath: "assets/images/salads/Greek.jpeg",
+      price: 1.89,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra olives", price: 0.50),
+      ],
+    ),
+    Food(
+      name: "Ham Salad",
+      description: "Ham Salad with lots of vegetables, olives and cheese.",
+      imagePath: "assets/images/salads/HamSalad.jpeg",
+      price: 3.00,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra olives", price: 0.50),
+      ],
+    ),
+    Food(
+      name: "Salad",
+      description: "Original vegetables salad.",
+      imagePath: "assets/images/salads/Salad.webp",
+      price: 2.00,
+      category: FoodCategory.salads,
+      availableAddons: [
+        Addon(name: "Extra olives", price: 0.50),
+      ],
+    ),
+    Food(
+      name: "Wurst Salad",
+      description: "Wurst and vegetable mix in the best way",
+      imagePath: "assets/images/salads/WurstSalad.jpeg",
       price: 1.89,
       category: FoodCategory.salads,
       availableAddons: [
